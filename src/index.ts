53b22d28ca1df1c539f8db980f16ecc184f020ed
andywindowsmac <adilkhankenzhetaev@gmail.com>
2018-03-15 18:16:12 +0600

    feat: parse spr

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,58 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import chalk from 'chalk';
-import { Application, Request, Response, NextFunction } from 'express';
+import * as windows1251 from 'windows-1251';
+import * as cheerio from 'cheerio';
+import * as request from 'request-promise';
 
-import RootRouter from './routes';
-
-const DEV_PORT = 3000;
-const PORT = process.env.PORT || DEV_PORT;
-const app: Application = express();
-
-// Load body parser to handle POST requests
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  }),
-);
-
-app.use('/api/v1/', RootRouter);
-
-/* eslint-disable no-console */
-app.listen(PORT, err => {
-  if (err) {
-    console.log(chalk.red(`Error ${err}`));
-    return;
+const string2Bin = str => {
+  var result = [];
+  for (var i = 0; i < str.length; i++) {
+    result.push(str.charCodeAt(i).toString(2));
   }
+  return result;
+};
 
-  console.log(chalk.green(`Express server running on port:  ${PORT}`));
-});
-/* eslint-enable no-console */
+const extractLinksFromHTML = (html: string): Array<string> => {
+  const $ = cheerio.load(html, { decodeEntities: true });
+  const listofA = $('a');
+  const result = Object.values(listofA)
+    .filter(
+      value =>
+        value &&
+        value.attribs &&
+        value.attribs.href &&
+        value.attribs.href.includes('//www.spr.kz/otzyvy/') &&
+        value.attribs.href.localeCompare('//www.spr.kz/otzyvy/') !== 0,
+    )
+    .map(value => value.attribs.href);
+
+  return result;
+};
+
+const extractComments = (html: string) => {
+  const $ = cheerio.load(html, { decodeEntities: true });
+  const td = $('td.valign').text('top');
+
+  var text = td
+    .contents()
+    .map(function() {
+      if (this.type === 'text')
+        return $(this)
+          .text()
+          .trim();
+    })
+    .get();
+  console.log(td);
+  return null;
+};
+
+const encodedQuery = windows1251.encode('Альфа банк Алматы');
+
+const testQuery = `https://www.spr.kz/res_new.php?findtext=${encodedQuery}&id_razdel_find=0&id_okrug_find=15`;
+
+(async () => {
+  const response = await request(testQuery);
+
+  const links = extractLinksFromHTML(response);
+  var commentsQuery = links[0].substr(2);
+  const firstFinded = await request(`https://${commentsQuery}`);
+  const comments = extractComments(firstFinded);
+})();
