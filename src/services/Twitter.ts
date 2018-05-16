6936de634e324f79ce699dccbafeab7251d00b5a
andywindowsmac <adilkhankenzhetaev@gmail.com>
2018-05-16 13:56:12 +0600

    feat(Twitter): extract full text

--- src/services/Twitter.ts
+++ src/services/Twitter.ts
@@ -1,4 +1,6 @@
 import * as Twit from "twit";
+import * as cheerio from "cheerio";
+import axios from "axios";
 
 const T = new Twit({
   consumer_key: "nAOGsRYeQMBA77QbbKBEuiGPY",
@@ -35,27 +37,49 @@ export interface TweetType {
   [key: string]: any;
 }
 
+const extractFullText = (html: string) => {
+  const $ = cheerio.load(html);
+  const text = $("div.js-tweet-text-container").text();
+  return text;
+};
+
+const getFullText = async (link: string) => {
+  try {
+    const { data } = await axios.get(link);
+    return extractFullText(data);
+  } catch (err) {
+    return null;
+  }
+};
+
 const prepareTweets = (tweets: Array<TweetType> | any) =>
-  tweets.map(tweet => {
-    const {
-      text,
-      created_at,
-      user: { name },
-      favorite_count,
-      retweet_count
-    } = tweet;
+  Promise.all(
+    tweets.map(async tweet => {
+      const {
+        text,
+        created_at,
+        user: { name },
+        favorite_count,
+        retweet_count
+      } = tweet;
 
-    const date = new Date(created_at).getTime() / 1000;
-    const extractLink = text.split("…")[1];
+      const date = new Date(created_at).getTime() / 1000;
+      const extractedLink = text.split("…")[1];
+      let fullText = "";
+      if (extractedLink) {
+        const fText = await getFullText(extractedLink);
+        fullText = fText.replace(/\t/g, "").replace(/\n/g, "");
+      }
 
-    return {
-      text,
-      date,
-      name,
-      likes: favorite_count,
-      reposts: retweet_count,
-      link: extractLink || null
-    };
-  });
+      return {
+        text: fullText || text,
+        date,
+        name,
+        likes: favorite_count,
+        reposts: retweet_count,
+        link: extractedLink || null
+      };
+    })
+  );
 
 export { getTweets, prepareTweets };
