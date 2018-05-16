4c2b827255d3abdbe848777c9165a7ba8372eafd
andywindowsmac <adilkhankenzhetaev@gmail.com>
2018-05-16 12:46:03 +0600

    fix: tslint issue

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
+app.use((_, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
